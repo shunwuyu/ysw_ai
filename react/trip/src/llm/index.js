7ec2ac4762bb1922f3f3955d9c8148d7a4e44717
shunwuyu <shunwu2001@163.com>
2025-07-25 21:21:44 +0800

    travel

--- react/trip/src/llm/index.js
+++ react/trip/src/llm/index.js
@@ -51,3 +51,12 @@ export const kimiChat = async (messages) => {
     )
     return res;
 }
+
+export const generateAvatar = async (text) => {
+    // 设计prompt 
+    const prompt = `
+    你是一位漫画设计师，需要为用户设计头像，主打奶龙风格。
+    用户的信息是${text}
+    要求有个性，有设计感。
+    `
+}
